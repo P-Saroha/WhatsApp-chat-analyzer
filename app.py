d835be97f8b7aba4dff072df13bf1a7b4b2e6318
P-Saroha <parveensaroha2002@gmail.com>
2025-04-02 21:18:19 +0530

    Added .gitignore and removed myenv from tracking

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import preprocessor , helper
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 st.sidebar.title("WhatsApp Chat Analyzer")
 
@@ -49,4 +51,59 @@ if uploaded_file is not None:
         # finding the busiest users in the groups
         if selected_user == "overall":
             st.subheader("Most Busiest Users")
-            col1, col2 = st.beta_columns(2)
+            x, new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+            
+            col1, col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values)
+                plt.xticks(rotation='vertical') 
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+        
+        # WordCloud
+        st.subheader("Word Cloud")
+        df_wc = helper.create_wordcloud(selected_user,df)
+        fig, ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+
+        ## most common words 
+
+        most_common_df = helper.most_common_words(selected_user,df)
+        st.subheader("Most Common Words")
+
+        fig, ax = plt.subplots(figsize=(14, 10))
+
+        ax.barh(most_common_df[0], most_common_df[1])
+        plt.xticks(rotation = 'vertical')
+
+        st.pyplot(fig)
+        # st.dataframe(most_common_df)
+
+        ## emojis analysis
+        emoji_df = helper.extract_emojis(selected_user,df)
+
+        st.subheader("Emojis Analysis")
+        col1,col2 = st.columns(2)
+
+        with col1:
+            st.dataframe(emoji_df)
+
+        top_5_emojis = emoji_df.head(5)  # Get top 5 emojis
+        # Set the font to one that supports emojis
+        mpl.rcParams['font.family'] = 'Noto Color Emoji'
+
+        with col2:
+            fig, ax = plt.subplots()
+            ax.pie(top_5_emojis[1], labels=top_5_emojis[0], autopct='%1.1f%%', startangle=90)
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+            plt.title('Top 5 Emojis')
+
+            # Display the pie chart
+            st.pyplot(fig)
+
+
--- helper.py
+++ helper.py
@@ -1,5 +1,11 @@
 from urlextract import URLExtract
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from collections import Counter
+import pandas as pd
+import emoji
 extractor = URLExtract()
+
 def fetch_stats(selected_user,df):
 
     if selected_user !="overall":
@@ -28,4 +34,52 @@ def fetch_stats(selected_user,df):
     return num_messages, len(words), no_of_media_messages, len(links)
 
         
+def most_busy_users(df):
+    # Get the top 5 users with the highest number of messages
+    x = df['user'].value_counts().head()
 
+    df = round((df['user'].value_counts()/df.shape[0])*100,2).reset_index().rename(columns={'count': 'percent' , 'user': 'name'})
+    return x,df
+    # name = x.index
+    # count = x.values
+    # plt.bar(name,count)
+
+
+def create_wordcloud(selected_user,df):
+    if selected_user !="overall":
+        df = df[df['user'] == selected_user]
+    
+    new_df = df[df['message'] != '<Media omitted>']
+
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    df_wc = wc.generate(new_df['message'].str.cat(sep=""))
+    return df_wc
+
+## most commont words
+def most_common_words(selected_user,df):
+    if selected_user !="overall":
+        df = df[df['user'] == selected_user]
+
+    new_df = df[df['message'] != '<Media omitted>']
+    words = []
+    for message in new_df['message']:
+        words.extend(message.split())
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(40))
+    return most_common_df
+
+## emojis function
+
+def extract_emojis(selected_user,df):
+    if selected_user !="overall":
+        df = df[df['user'] == selected_user]
+
+    emojis = []
+    for message in df['message']:
+        emojis.extend([c for c in message if emoji.is_emoji(c)])
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+    return emoji_df
+    
+
+    
